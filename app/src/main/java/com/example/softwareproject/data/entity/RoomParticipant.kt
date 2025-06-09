b71b3a9baff569ee123544773883b7062ebe18a2
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-09 17:03:41 +0900

    feat: RoomParticipant 엔티티 (#1)

--- app/src/main/java/com/example/softwareproject/data/entity/RoomParticipant.kt
+++ app/src/main/java/com/example/softwareproject/data/entity/RoomParticipant.kt
@@ -1,4 +1,43 @@
 package com.example.softwareproject.data.entity
 
-class RoomParticipant {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.example.softwareproject.data.entity.base.BaseEntity
+import com.example.softwareproject.data.entity.base.BattleStats
+
+@Entity(tableName = "room_participant",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["user_id"],
+            childColumns = ["user_id"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Room::class,
+            parentColumns = ["room_id"],
+            childColumns = ["room_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+
+)
+data class RoomParticipant (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "room_participant_id")
+    val id : Int,
+    @ColumnInfo(name = "room_id")
+    val roomId : Int,
+    @ColumnInfo(name = "user_id")
+    val userId : Int,
+    @ColumnInfo(name = "solved_problem")
+    var solvedProblem : Int,
+    @Embedded
+    var battleStats: BattleStats = BattleStats(),
+    @Embedded
+    var baseEntity: BaseEntity = BaseEntity(),
+    )
