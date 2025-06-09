43dd709b244c233bc9d2d3c3c3277cf5d32d8db9
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-09 17:06:51 +0900

    feat: CsRoom 엔티티 (#1)

--- app/src/main/java/com/example/softwareproject/data/entity/CsRoom.kt
+++ app/src/main/java/com/example/softwareproject/data/entity/CsRoom.kt
@@ -1,4 +1,32 @@
 package com.example.softwareproject.data.entity
 
-class csRoom {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.example.softwareproject.data.entity.base.DifficultyCs
+import com.example.softwareproject.data.entity.base.Topic
+
+@Entity(tableName = "cs_room",
+    foreignKeys = [
+        ForeignKey(
+            entity = Room::class,
+            parentColumns = ["room_id"],
+            childColumns = ["room_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index(value = ["room_id"], unique = true)]
+)
+data class CsRoom (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "cs_room_id")
+    val id : Int,
+    @ColumnInfo(name = "room_id")
+    val roomId : Int,
+    @ColumnInfo(name = "topic")
+    var topic : Topic,
+    @ColumnInfo(name = "difficulty_level")
+    var difficultyCs: DifficultyCs
+)
