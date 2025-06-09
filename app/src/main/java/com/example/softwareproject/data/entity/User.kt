768069ca95ba40f196af594cd883f897732a7df3
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-09 16:59:33 +0900

    feat: User엔티티 등록 (#1)

--- app/src/main/java/com/example/softwareproject/data/entity/User.kt
+++ app/src/main/java/com/example/softwareproject/data/entity/User.kt
@@ -1,4 +1,17 @@
 package com.example.softwareproject.data.entity
 
-class User {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.softwareproject.data.entity.base.BaseEntity
+
+@Entity(tableName = "user")
+data class User(
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "user_id")
+    val id : Int,
+    @Embedded
+    var baseEntity : BaseEntity = BaseEntity()
+)
