f92223989dc7307487365a40be098092e57b25dc
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-09 17:07:15 +0900

    feat: CodingRoom 엔티티 (#1)

--- app/src/main/java/com/example/softwareproject/data/entity/CodingRoom.kt
+++ app/src/main/java/com/example/softwareproject/data/entity/CodingRoom.kt
@@ -1,4 +1,32 @@
 package com.example.softwareproject.data.entity
 
-class CodingRoom {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.example.softwareproject.data.entity.base.BaekjoonTier
+
+@Entity(tableName = "coding_room",
+    foreignKeys = [
+        ForeignKey(
+            entity = Room::class,
+            parentColumns = ["room_id"],
+            childColumns = ["room_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index(value = ["room_id"], unique = true)]
+)
+
+data class CodingRoom (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "coding_room_id")
+    val id : Int,
+
+    @ColumnInfo(name = "room_id")
+    val roomId : Int,
+
+    @ColumnInfo(name = "difficulty_level")
+    var difficulty : BaekjoonTier
+)
