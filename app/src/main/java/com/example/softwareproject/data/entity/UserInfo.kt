5b8f28c6eb0be3f0dbf0bfd81c652399e448c711
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-09 17:00:02 +0900

    feat: UserInfo엔티티 등록 (#1)

--- app/src/main/java/com/example/softwareproject/data/entity/UserInfo.kt
+++ app/src/main/java/com/example/softwareproject/data/entity/UserInfo.kt
@@ -1,4 +1,33 @@
 package com.example.softwareproject.data.entity
 
-class UserInfo {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.example.softwareproject.data.entity.base.BaseEntity
+
+@Entity(tableName = "user_info",
+        foreignKeys = [
+            ForeignKey(
+                entity = User::class,
+                parentColumns = ["user_id"],
+                childColumns = ["user_id"],
+                onDelete = ForeignKey.CASCADE
+            )
+        ],
+    indices = [Index(value = ["user_id"], unique = true)]
+    )
+data class UserInfo (
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "user_info_id")
+    val id : Int,
+
+    @ColumnInfo(name = "user_id")
+    val userId : Int,
+
+    @Embedded
+    var baseEntity : BaseEntity = BaseEntity()
+)
