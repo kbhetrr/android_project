71433ee3c20775f0763dac5af0e88ac552c4e839
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-09 17:03:04 +0900

    feat: Room 엔티티 (#1)

--- app/src/main/java/com/example/softwareproject/data/entity/Room.kt
+++ app/src/main/java/com/example/softwareproject/data/entity/Room.kt
@@ -1,11 +1,30 @@
-package com.example.softwareproject.data.entity.base
+package com.example.softwareproject.data.entity
 
 import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import com.example.softwareproject.data.entity.base.BaseEntity
+import com.example.softwareproject.data.entity.base.RoomState
+import com.example.softwareproject.data.entity.base.RoomType
+
+
 
 data class Room (
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "room_id")
-    val id : Int
-    room_
+    val id : Int,
+    @ColumnInfo(name="room_title")
+    val roomTitle : String,
+    @ColumnInfo(name = "problem_count")
+    val problemCount : Int,
+    @ColumnInfo(name = "room_type")
+    val roomType: RoomType,
+    @ColumnInfo(name = "room_state")
+    val roomState: RoomState,
+    @ColumnInfo(name = "description")
+    val description : String,
+    @Embedded
+    var baseEntity : BaseEntity = BaseEntity()
 )
