7cf9f5e5cda485e45ada8b8549b923abecc9b241
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-09 17:08:16 +0900

    feat: ParticipantProblemStatus (#1)

--- app/src/main/java/com/example/softwareproject/data/entity/ParticipantProblemStatus.kt
+++ app/src/main/java/com/example/softwareproject/data/entity/ParticipantProblemStatus.kt
@@ -1,4 +1,32 @@
 package com.example.softwareproject.data.entity
 
-class ParticipantProblemStatus {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import com.example.softwareproject.data.entity.base.RoomType
+@Entity(tableName = "participant_problem_status",
+    foreignKeys = [
+        ForeignKey(
+            entity = RoomParticipant::class,
+            parentColumns = ["room_participant_id"],
+            childColumns = ["room_participant_id"],
+            onDelete = ForeignKey.CASCADE
+        ),
+    ],
+
+)
+
+data class ParticipantProblemStatus (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "participant_problem_status")
+    val id : Int,
+    @ColumnInfo(name = "room_participant_id")
+    val roomParticipantId : Int,
+    @ColumnInfo(name = "problem_id")
+    var problemId : Int,
+    @ColumnInfo(name = "problem_type")
+    var problemType : RoomType,
+    @ColumnInfo(name = "is_solved")
+    val isSolved : Boolean
+)
