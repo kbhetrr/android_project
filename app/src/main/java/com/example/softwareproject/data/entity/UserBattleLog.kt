d3ef100f23b26bea4e909f7a1c70732d7676c1da
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-09 17:02:08 +0900

    feat: UserBattleLog 엔티티 (#1)

--- app/src/main/java/com/example/softwareproject/data/entity/UserBattleLog.kt
+++ app/src/main/java/com/example/softwareproject/data/entity/UserBattleLog.kt
@@ -1,4 +1,41 @@
 package com.example.softwareproject.data.entity
 
-class UserBattleLog {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.example.softwareproject.data.entity.base.BaseEntity
+
+@Entity(tableName = "user_battle_log",
+    foreignKeys = [
+        ForeignKey(
+            entity = UserInfo::class,
+            parentColumns = ["user_info_id"],
+            childColumns = ["user_info_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index(value = ["user_info_id"], unique = true)]
+)
+
+data class UserBattleLog (
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "user_battle_log_id")
+    val id : Int,
+
+    @ColumnInfo(name = "user_info_id")
+    val userInfoId : Int,
+    @ColumnInfo(name = "match")
+    var match : Int,
+    @ColumnInfo(name = "win")
+    var win : Int,
+    @ColumnInfo(name = "lose")
+    var lose : Int,
+    @ColumnInfo(name = "rate")
+    var rate: Double,
+    @Embedded
+    var baseEntity: BaseEntity = BaseEntity()
+)
