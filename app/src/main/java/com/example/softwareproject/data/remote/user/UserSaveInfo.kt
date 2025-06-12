41e1c1f00a9bee18b01f606bffc7d5d19a33e1f2
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-12 18:44:43 +0900

    feat: 깃허브 로그인 로직 작성(#2)

---
+++ app/src/main/java/com/example/softwareproject/data/remote/user/UserSaveInfo.kt
@@ -0,0 +1,4 @@
+package com.example.softwareproject.com.example.softwareproject.data.remote.user
+
+class UserSaveInfo {
+}
--- app/src/main/java/com/example/softwareproject/domain/repository/UserRepository.kt
+++ app/src/main/java/com/example/softwareproject/domain/repository/UserRepository.kt
@@ -1,7 +1,10 @@
 package com.example.softwareproject.domain.repository
 
-import com.example.softwareproject.data.entity.User
+import com.example.softwareproject.com.example.softwareproject.data.remote.user.UserFullInfo
+import com.example.softwareproject.com.example.softwareproject.data.remote.user.UserSaveInfo
 
 interface UserRepository {
-    suspend fun getUserInfo(userId: Int): User
+    suspend fun getUserInfo(userId: String) : UserFullInfo
+    suspend fun createUserInfo(userSaveInfo:UserSaveInfo)
+    suspend fun isUserExists(userId: String): Boolean
 }
