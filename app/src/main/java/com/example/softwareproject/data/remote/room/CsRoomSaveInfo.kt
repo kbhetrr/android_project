f4fae2ac20c8e5875f8e5b04793da7157ae7a937
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-13 23:47:04 +0900

    feat: 방생성을 하기 위한 정보를 받아오는 DTO (#15)

--- app/src/main/java/com/example/softwareproject/data/remote/room/CsRoomSaveInfo.kt
+++ app/src/main/java/com/example/softwareproject/data/remote/room/CsRoomSaveInfo.kt
@@ -1,10 +1,12 @@
 package com.example.softwareproject.com.example.softwareproject.data.remote.room
 
+import com.example.softwareproject.util.DifficultyCs
 import com.example.softwareproject.util.RoomState
 import com.example.softwareproject.util.RoomType
+import com.example.softwareproject.util.Topic
 import com.google.firebase.Timestamp
 
-data class RoomSaveInfo (
+data class CsRoomSaveInfo (
     val roomId: String,
     val description: String,
     val problemCount: Int,
@@ -12,6 +14,8 @@ data class RoomSaveInfo (
     val roomTitle: String,
     val roomType: RoomType,
     val userId: String,
+    val topic: Topic,
+    var difficultyLevel: DifficultyCs,
     val createdAt: Timestamp,
     val updatedAt: Timestamp
 )
