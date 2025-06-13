9bfaf76b9891fe2419e31d1fbd328dbfd5488340
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-13 23:47:53 +0900

    feat: 게임방과 관련된 함수 정리 (#15)
    - 방정보 가져오기 getRoomInfo
    - CS대전방 생성
    - Coding대전방 생성
    - CsRoom 방 리스트 반환
    - CodingRoom 방 리스트 반환

--- app/src/main/java/com/example/softwareproject/domain/repository/RoomRepository.kt
+++ app/src/main/java/com/example/softwareproject/domain/repository/RoomRepository.kt
@@ -1,4 +1,14 @@
 package com.example.softwareproject.com.example.softwareproject.domain.repository
 
+import com.example.softwareproject.com.example.softwareproject.data.remote.room.CodingRoomSaveInfo
+import com.example.softwareproject.com.example.softwareproject.data.remote.room.CsRoomSaveInfo
+import com.example.softwareproject.com.example.softwareproject.data.remote.room.UiCodingRoomItem
+import com.example.softwareproject.com.example.softwareproject.data.remote.room.UiCsRoomItem
+
 interface RoomRepository {
+    suspend fun getRoomInfo(roomId: String)
+    suspend fun createCsRoom(csRoomSaveInfo:  CsRoomSaveInfo)
+    suspend fun createCodingRoom(codingRoomSaveInfo: CodingRoomSaveInfo)
+    suspend fun listCsRoom() : List<UiCsRoomItem>
+    suspend fun listCodingRoom() : List<UiCodingRoomItem>
 }
