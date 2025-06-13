650d53d985b4796ef2ba3bcf18cd351918e841db
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-13 23:44:29 +0900

    feat: 백준 문제를 가지고 오기 위한 interface(#15)

--- app/src/main/java/com/example/softwareproject/domain/repository/solvedac/SolvedAcApi.kt
+++ app/src/main/java/com/example/softwareproject/domain/repository/solvedac/SolvedAcApi.kt
@@ -1,6 +1,11 @@
-package com.example.softwareproject.com.example.softwareproject.domain.repository
+package com.example.softwareproject.com.example.softwareproject.domain.repository.solvedac
 
 import com.google.gson.annotations.SerializedName
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -23,9 +28,22 @@ data class SolvedAcProblem(
     @SerializedName("titleKo") val title: String,
     @SerializedName("level") val tier: Int,
     @SerializedName("tags") val tags: List<SolvedAcTag>,
-    @SerializedName("isSolvable") val isSolvable: Boolean
+    @SerializedName("isSolvable") val isSolvable: Boolean,
+    @SerializedName("acceptedUserCount") val acceptedUserCount: Int?
 )
 
 data class SolvedAcTag(
     @SerializedName("key") val key: String
 )
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitInstance {
+    val solvedAcApi: SolvedAcApi by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://solved.ac/api/v3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(SolvedAcApi::class.java)
+    }
+}
