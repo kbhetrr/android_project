4d97539d54f934037cf71a81831fae45b898d3e9
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-19 19:38:20 +0900

    feat: 로그인 화면 첫페이지 추가

--- app/src/main/java/com/example/softwareproject/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/example/softwareproject/presentation/login/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.softwareproject.com.example.softwareproject.presentation.login
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.example.softwareproject.MainActivity
 import com.example.softwareproject.R
 import com.example.softwareproject.databinding.LoginBinding
 import com.example.softwareproject.presentation.login.LoginViewModel
@@ -42,6 +44,10 @@ class LoginFragment : Fragment(R.layout.login) {
                         Log.d("Login", "신규 유저 생성 완료")
                     }
                     Log.d("Login", "로그인 성공: ${user.email}")
+
+                    val intent = Intent(requireContext(), MainActivity::class.java)
+                    startActivity(intent)
+                    requireActivity().finish()
                 }
             } else {
                 Log.e("Login", "로그인 실패 또는 취소")
