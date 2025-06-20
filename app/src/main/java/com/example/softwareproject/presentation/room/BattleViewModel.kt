ab0b291ed3548f2f64cbae40e57d8c7b33495451
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-21 02:51:59 +0900

    feat: BattleViewModel 추가(#20)

--- app/src/main/java/com/example/softwareproject/presentation/room/BattleViewModel.kt
+++ app/src/main/java/com/example/softwareproject/presentation/room/BattleViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.softwareproject.com.example.softwareproject.presentation.room
 
-class BattleViewModel {
+import android.view.View
+import androidx.lifecycle.ViewModel
+import com.example.softwareproject.com.example.softwareproject.domain.usecase.room.BattleUseCase
+import com.example.softwareproject.domain.usecase.room.RoomUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BattleViewModel @Inject constructor(
+    private val roomUseCase: RoomUseCase,
+    private val battleUseCase: BattleUseCase
+) :ViewModel()
+{
+    suspend fun createCsProblem(roomId : String){
+        battleUseCase.createCsProblem(roomId)
+    }
 }
