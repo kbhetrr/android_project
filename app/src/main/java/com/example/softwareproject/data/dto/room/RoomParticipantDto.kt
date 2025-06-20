a5cddf6bf5cd288abee90fed1458960423654163
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-20 15:38:45 +0900

    feat: RoomParticipantDto 추가 (#27)

--- app/src/main/java/com/example/softwareproject/data/dto/room/RoomParticipantDto.kt
+++ app/src/main/java/com/example/softwareproject/data/dto/room/RoomParticipantDto.kt
@@ -1,4 +1,17 @@
-package com.example.softwareproject.com.example.softwareproject.data.dto.room
+package com.example.softwareproject.data.dto.room
 
-class RoomParticipantDto {
-}
+import com.example.softwareproject.util.UserRole
+import com.google.firebase.Timestamp
+import java.sql.Time
+
+data class RoomParticipantDto (
+    val userId: String,
+    val attack: Int,
+    val hp: Int,
+    val shield: Int,
+    val role : UserRole,
+    val roomId: String,
+    val solvedProblem: Int,
+    val createdAt: Timestamp,
+    val updatedAt: Timestamp,
+)
