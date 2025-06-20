fb519caf6f91cb94acca8ec924f93b2543a8f269
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-21 02:49:38 +0900

    feat: BattleWaitingActivity 추가(#20)

--- app/src/main/java/com/example/softwareproject/presentation/BattleWaitingActivity.kt
+++ app/src/main/java/com/example/softwareproject/presentation/BattleWaitingActivity.kt
@@ -1,4 +1,58 @@
 package com.example.softwareproject.com.example.softwareproject.presentation
 
-class BattleWaitingActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.softwareproject.CsBattleActivity
+import com.example.softwareproject.R
+import com.example.softwareproject.com.example.softwareproject.domain.usecase.room.BattleUseCase
+import com.example.softwareproject.com.example.softwareproject.presentation.room.BattleViewModel
+import com.example.softwareproject.presentation.room.RoomViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class BattleWaitingActivity : AppCompatActivity(){
+    private val roomViewModel: RoomViewModel by viewModels()
+    private val battleViewModel : BattleViewModel by viewModels()
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_battle_waiting)
+
+        val roomId = intent.getStringExtra("roomId") ?: return
+
+
+        lifecycleScope.launch {
+            try {
+
+                roomViewModel.battleStart(roomId)
+
+                battleViewModel.createCsProblem(roomId)
+                val intent = Intent(this@BattleWaitingActivity, CsBattleActivity::class.java)
+                intent.putExtra("roomId", roomId)
+                startActivity(intent)
+                finish()
+
+            } catch (e: Exception) {
+                Log.e("BattleWaiting", "문제 생성 실패: ${e.message}")
+                Toast.makeText(this@BattleWaitingActivity, "문제 생성 실패", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+
+    }
+
+
+    override fun onBackPressed() {
+        super.onBackPressed() // 기본 동작 (finish() 호출)
+    }
 }
