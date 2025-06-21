f57ef2babf1e232de0ad4723aa8350f27a883b25
kbhetrr <kimkh7534@naver.com>
2025-06-21 19:34:23 +0900

    KKH: Battle Result Screen 구현 완료

---
+++ app/src/main/java/com/example/softwareproject/ResultActivity.kt
@@ -0,0 +1,38 @@
+package com.example.softwareproject // 실제 패키지 이름으로 변경
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity // AppCompatActivity 상속
+import androidx.lifecycle.lifecycleScope
+import com.example.softwareproject.com.example.softwareproject.presentation.room.viewmodel.RoomViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class ResultActivity : AppCompatActivity() {
+
+//    private val viewModel: RoomViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result) // activity_battle_loading.xml 설정
+
+//        val btnGoBack: Button = findViewById(R.id.btn_go_back)
+//        val roomId = intent.getStringExtra("roomId") ?: return
+//
+//        btnGoBack.setOnClickListener {
+//            lifecycleScope.launch {
+//                viewModel.deleteRoom(roomId)
+//                Log.d("RoomViewModel", "방 삭제 완료: $roomId")
+//                finish()
+//            }
+//        }
+    }
+
+    // 사용자가 시스템의 뒤로가기 버튼을 눌렀을 때도 finish()와 동일하게 동작
+//    override fun onBackPressed() {
+//        super.onBackPressed() // 기본 동작 (finish() 호출)
+//    }
+}
