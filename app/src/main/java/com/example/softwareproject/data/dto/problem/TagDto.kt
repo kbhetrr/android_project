d03b43a26aa68ffd27d6a2e608c7adb77605a9ed
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-21 12:17:58 +0900

    feat: BeakjoonApi, TagDto생성(#20)

--- app/src/main/java/com/example/softwareproject/data/dto/problem/TagDto.kt
+++ app/src/main/java/com/example/softwareproject/data/dto/problem/TagDto.kt
@@ -1,4 +1,5 @@
 package com.example.softwareproject.com.example.softwareproject.data.dto.problem
 
-class TagDto {
-}
+data class TagDto(
+    val key: String
+)
--- app/src/main/java/com/example/softwareproject/module/BaekjoonApi.kt
+++ app/src/main/java/com/example/softwareproject/module/BaekjoonApi.kt
@@ -1,14 +1,18 @@
 package com.example.softwareproject.com.example.softwareproject.module
 
-import androidx.room.Query
+
+import com.example.softwareproject.data.dto.problem.BaekjoonApiResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface BaekjoonApi {
     @GET("v1/problem/search")
     suspend fun getProblemsByTag(
-        @Query("query") query: String,
         @Query("tierFrom") tierFrom: Int,
         @Query("tierTo") tierTo: Int,
         @Query("page") page: Int
     ): BaekjoonApiResponse
+
+
+
 }
